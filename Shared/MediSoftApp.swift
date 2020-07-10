b1e22ca6d096935f275daf8fb656b7a451a00b03
Guillermo Bernal <gbernal096@gmail.com>
2020-07-10 15:57:06 -0500

    Initial Commit

---
+++ Shared/MediSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediSoftApp.swift
+//  Shared
+//
+//  Created by Guillermo Bernal on 7/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MediSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
